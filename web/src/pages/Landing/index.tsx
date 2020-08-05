c838c0be9fb8aab8f190a2e65edb08ee23543076
th92rodr <thiago.clrodrigues@gmail.com>
2020-08-05 10:42:01 -0300

    update Landing Page hrefs for react-router-dom Links

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import LogoImg from '../../assets/images/logo.svg';
 import LandingImg from '../../assets/images/landing.svg';
@@ -24,13 +25,13 @@ function Landing() {
         />
 
         <div className='buttons-container'>
-          <a href='' className='study'>
+          <Link to='/study' className='study'>
             <img src={StudyIcon} alt='Estudar' /> Estudar
-          </a>
+          </Link>
 
-          <a href='' className='give-classes'>
+          <Link to='/give-classes' className='give-classes'>
             <img src={GiveClassesIcon} alt='Dar aulas' /> Dar aulas
-          </a>
+          </Link>
         </div>
 
         <span className='total-connections'>
