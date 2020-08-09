a2911173759423517faabb3ecb84a86791b26b98
th92rodr <thiago.clrodrigues@gmail.com>
2020-08-09 14:09:29 -0300

    create PageHeader Component

---
+++ mobile/src/components/PageHeader/index.tsx
@@ -0,0 +1,47 @@
+import React, { ReactNode } from 'react';
+import { View, Image, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { BorderlessButton } from 'react-native-gesture-handler';
+
+import LogoImg from '../../assets/images/logo.png';
+import BackIcon from '../../assets/images/icons/back.png';
+
+import styles from './styles';
+
+interface PageHeaderProps {
+  title: string;
+  headerRight?: ReactNode;
+}
+
+const PageHeader: React.FC<PageHeaderProps> = ({
+  title,
+  headerRight,
+  children,
+}) => {
+  const { navigate } = useNavigation();
+
+  function handleGoBack() {
+    navigate('Landing');
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topBar}>
+        <BorderlessButton onPress={handleGoBack}>
+          <Image source={BackIcon} resizeMode='contain' />
+        </BorderlessButton>
+
+        <Image source={LogoImg} resizeMode='contain' />
+      </View>
+
+      <View style={styles.header}>
+        <Text style={styles.title}>{title}</Text>
+        {headerRight}
+      </View>
+
+      {children}
+    </View>
+  );
+};
+
+export default PageHeader;
